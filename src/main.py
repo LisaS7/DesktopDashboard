4d02020bf14811ff8157e072cac0b74180d659d0
Lisa <84908214+LisaS7@users.noreply.github.com>
2024-04-21 10:16:56 +0100

    Fixing update functionality to loop every second and call recursively

--- src/main.py
+++ src/main.py
@@ -6,19 +6,19 @@ import config
 class App(ttkb.Window):
     def __init__(self):
         super().__init__(themename=config.THEME_NAME)
-        self.stat_frame = None
+        self.stat_frame = StatWidget(master=self)
+        self.after(1000, self.update)
         self.layout()
-        self.add_widgets()
-        
+
     def layout(self):
         self.geometry(f"{config.WINDOW_WIDTH}x{config.WINDOW_HEIGHT}")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
-
-    def add_widgets(self):
-        self.stat_frame = StatWidget(master=self)
         self.stat_frame.grid(row=0, column=0, columnspan=5, padx=20, pady=20, sticky="nsew")
-        self.after(1000, self.stat_frame.update)
+
+    def update(self):
+        self.stat_frame.update()
+        self.after(1000, self.update)
 
 
 window = App()
